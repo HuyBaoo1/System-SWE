5bf9137dd415a178583e09b3b905ddf957f2d35e
HuyBaoo1 <10421020@student.vgu.edu.vn>
2024-05-24 00:14:57 +0200

    Create User.java

---
+++ User.java
@@ -0,0 +1,18 @@
+public class User {
+    private String name;
+    private String email;
+
+    // Constructors, getters, and setters
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
